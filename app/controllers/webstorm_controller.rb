21186b7cd293f657e995983f302a64c88c7ac425
timellado <timellado@uc.cl>
2018-12-12 17:30:09 -0300

    view webstorm

---
+++ app/controllers/webstorm_controller.rb
@@ -0,0 +1,6 @@
+class WebstormController < ApplicationController
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
 	get '/', to: 'api/api#token'
+
+	get '/web', to: 'webstorm#index'
+
 end
